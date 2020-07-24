516b6a9d59c94594914c277e61380b874ef0b84b
Pat McClernan <patmccler@gmail.com>
2020-07-24 09:52:40 -0400

    refactored most ui functoinality relating to rendering a level into level class and created a UIController class

--- map_editor_frontend/index.js
+++ map_editor_frontend/index.js
@@ -30,36 +30,93 @@ class Level {
   }
 
   addTile(x,y) {
+    console.log(this)
+    console.log(this.tiles)
     this.tiles.push(new Tile(x,y))
   }
 
   removeTile(tile) {
     this.tiles.splice(this.tiles.indexOf(tile),1)
   }
-}
 
-Level.prototype.render = function() {
-  renderLevel(this)
-  this.renderable = false
+  render() {
+    let mapDiv = document.querySelector("#map")
+    mapDiv.innerHTML = ""
+    mapDiv.appendChild(this.buildHeaderRow(this.width))
+
+    for(let r = 0; r < this.height; r++) {
+      mapDiv.appendChild(this.buildLevelRow(r, this.width))
+    }
+
+    this.renderLevelName()
+    this.renderLevelWidth()
+    this.renderLevelHeight()
+    this.renderable = false
+  }
+
+  renderLevelName() {
+    document.getElementById("level-name").innerText = this.name
+  }
+
+  renderLevelWidth() {
+    document.getElementById("level-width").innerText = this.width
+  }
+
+  renderLevelHeight() {
+    document.getElementById("level-height").innerText = this.height
+  }
+
+  buildLevelRow(row, columns) {
+    let mapRowDiv = document.createElement("div")
+    mapRowDiv.classList.add("map-row")
+    mapRowDiv.appendChild(this.headerTileTemplate(row))
+    for(let col = 0; col < columns; col++) {
+      mapRowDiv.appendChild(this.tileTemplate(col, row))
+    }
+    return mapRowDiv
+  }
+
+  tileTemplate(col, row) {
+    let tileDiv = document.createElement("div")
+    tileDiv.classList.add("tile")
+    tileDiv.addEventListener("click", e => this.tileClicked(col, row))
+    return tileDiv
+  }
+
+  tileClicked(col, row) {
+    return this.toggleTile(col,row)
+  }
+
+  headerTileTemplate(index) {
+    let div = document.createElement("div")
+    div.innerText = index + 1
+    return div
+  }
+
+  buildHeaderRow(length) {
+    let mapRowDiv = document.createElement("div")
+    mapRowDiv.classList.add("map-row")
+
+    for(let i = 0; i <= length; i++) {
+      let div = document.createElement("div")
+      div.innerText = i
+      div.classList.add("tile", "map-header")
+      mapRowDiv.appendChild(div)
+    }
+    return mapRowDiv
+  }
 }
 
 class Tile {
   constructor(x,y) {
     this.x = x
     this.y = y
-
-    this.constructor.allTiles.push(this)
   }
 
   static findTile(tiles,x,y) {
     // probably a faster way to do this, may need to look into later
     return tiles.find(tile => tile.x === x && tile.y === y)
   }
-
-  static findOrCreateTile(x,y) {
-    return this.findTile(x,y) || new Tile(x,y)
-  }
-
 }
 
 /** This is where some setup happens
@@ -74,6 +131,7 @@ document.addEventListener("DOMContentLoaded", e => {
     'Content-Type': 'application/json',
     "Accept": "application/json"
   }
+  const UI_CONTROLLER = new UIController()
 
   let levels;
   let currentLevel;
@@ -94,7 +152,9 @@ document.addEventListener("DOMContentLoaded", e => {
       levels = allLevels.map(level => {
         return Object.assign(new Level(), level)
       })
-      populateLevelSelect(levels)
+      UI_CONTROLLER.levels = levels
+      UI_CONTROLLER.populateLevelSelect(levels)
+      console.log(UI_CONTROLLER)
       chooseLevel(levels[0].id)
     })
   }
@@ -107,12 +167,18 @@ document.addEventListener("DOMContentLoaded", e => {
   document.querySelector("#levels-select").addEventListener("change", e => chooseLevel(parseInt(e.target.value)))
 })
 
-  function populateLevelSelect(levels) {
-    let levelSelect = document.querySelector("#levels-select")
-    levels.forEach(level => levelSelect.appendChild(buildLevelOption(level)))
+class UIController {
+  constructor(levels, current) {
+    this.levels = levels
+    this.currentLevel = current
   }
 
-  function buildLevelOption(level) {
+  populateLevelSelect(levels) {
+    let levelSelect = document.querySelector("#levels-select")
+    levels.forEach(level => levelSelect.appendChild(this.buildLevelOption(level)))
+  }
+
+  buildLevelOption(level) {
     let opt = document.createElement("option")
     opt.value = level.id
     opt.innerText = level.name
@@ -120,68 +186,5 @@ document.addEventListener("DOMContentLoaded", e => {
   }
 
 
-  function renderLevel(level) {
-    let mapDiv = document.querySelector("#map")
-    mapDiv.innerHTML = ""
-    mapDiv.appendChild(buildHeaderRow(level.width))
 
-    for(let r = 0; r < level.height; r++) {
-      mapDiv.appendChild(buildLevelRow(r, level.width))
-    }
-
-    renderLevelName(level.name)
-    renderLevelWidth(level.width)
-    renderLevelHeight(level.height)
-  }
-
-  function renderLevelName(name) {
-    document.getElementById("level-name").innerText = name
-  }
-
-  function renderLevelWidth(width) {
-    document.getElementById("level-width").innerText = width
-  }
-
-  function renderLevelHeight(height) {
-    document.getElementById("level-height").innerText = height
-  }
-
-  function buildLevelRow(row, columns) {
-    let mapRowDiv = document.createElement("div")
-    mapRowDiv.classList.add("map-row")
-    mapRowDiv.appendChild(headerTileTemplate(row))
-    for(let col = 0; col < columns; col++) {
-      mapRowDiv.appendChild(tileTemplate(col, row))
-    }
-    return mapRowDiv
-  }
-
-  function tileTemplate(col, row) {
-    let tileDiv = document.createElement("div")
-    tileDiv.classList.add("tile")
-    tileDiv.addEventListener("click", e => tileClicked(col, row))
-    return tileDiv
-  }
-
-  function tileClicked(col, row) {
-    return currentLevel.toggleTile(col,row)
-  }
-
-  function headerTileTemplate(index) {
-    let div = document.createElement("div")
-    div.innerText = index + 1
-    return div
-  }
-
-  function buildHeaderRow(length) {
-    let mapRowDiv = document.createElement("div")
-    mapRowDiv.classList.add("map-row")
-
-    for(let i = 0; i <= length; i++) {
-      let div = document.createElement("div")
-      div.innerText = i
-      div.classList.add("tile", "map-header")
-      mapRowDiv.appendChild(div)
-    }
-    return mapRowDiv
-  }
+}
