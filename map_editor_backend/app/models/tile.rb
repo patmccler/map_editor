bc76520205bef1f79041a7d63e72f29d23682eef
Pat McClernan <patmccler@gmail.com>
2020-07-24 13:27:20 -0400

    fix validation for tile range

--- map_editor_backend/app/models/tile.rb
+++ map_editor_backend/app/models/tile.rb
@@ -1,4 +1,4 @@
 class Tile < ApplicationRecord
   belongs_to :level
-  validates :x, :y, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :x, :y, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
